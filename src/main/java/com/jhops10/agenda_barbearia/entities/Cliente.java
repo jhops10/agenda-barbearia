0a2d9238d4ba2ab957335bdfdef473022aeeb914
João Paulo <joaowais@gmail.com>
2025-03-29 20:49:32 -0300

    adiciona entidade Cliente

---
+++ src/main/java/com/jhops10/agenda_barbearia/entities/Cliente.java
@@ -0,0 +1,69 @@
+package com.jhops10.agenda_barbearia.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
