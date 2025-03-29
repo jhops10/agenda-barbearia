d3de7e4a01e5f5a878f06943c6d92dc827e3b7ea
João Paulo <joaowais@gmail.com>
2025-03-29 20:25:48 -0300

    first commit

---
+++ src/main/java/com/jhops10/agenda_barbearia/AgendaBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.agenda_barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBarbeariaApplication.class, args);
+	}
+
+}
