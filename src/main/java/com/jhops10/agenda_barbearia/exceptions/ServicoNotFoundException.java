fb527062e4bc94750fcb7920f5133f2254723933
João Paulo <joaowais@gmail.com>
2025-03-31 19:16:47 -0300

    adiciona ServicoNotFoundException

---
+++ src/main/java/com/jhops10/agenda_barbearia/exceptions/ServicoNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jhops10.agenda_barbearia.exceptions;
+
+public class ServicoNotFoundException extends RuntimeException {
+
+    public ServicoNotFoundException(String message) {
+        super(message);
+    }
+}
