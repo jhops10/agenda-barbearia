83a0389de8b6874c0e9f202827e5368f57834228
João Paulo <joaowais@gmail.com>
2025-04-01 21:42:27 -0300

    atualiza a entidade Servico com JsonIgnoreProperties

--- src/main/java/com/jhops10/agenda_barbearia/entities/Servico.java
+++ src/main/java/com/jhops10/agenda_barbearia/entities/Servico.java
@@ -1,10 +1,12 @@
 package com.jhops10.agenda_barbearia.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -27,8 +29,9 @@ public class Servico {
     @Column(nullable = false)
     private BigDecimal preco;
 
-    @ManyToMany(mappedBy = "servicos")
-    private List<Barbeiro> barbeiros;
+    @ManyToMany(mappedBy = "servicos", fetch = FetchType.EAGER)
+    @JsonIgnoreProperties("servicos")
+    private List<Barbeiro> barbeiros = new ArrayList<>();
 
     public Servico() {
     }
